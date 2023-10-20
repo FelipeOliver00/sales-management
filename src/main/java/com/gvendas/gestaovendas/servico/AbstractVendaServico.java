1e6adfb7075bf9816386051cf5053a8dd167eddf
Felipe <felipecastro0208@hotmail.com>
2023-10-20 17:37:52 -0300

    Created an AbstractVendaServico class

---
+++ src/main/java/com/gvendas/gestaovendas/servico/AbstractVendaServico.java
@@ -0,0 +1,22 @@
+package com.gvendas.gestaovendas.servico;
+
+import com.gvendas.gestaovendas.dto.venda.ItemVendaResponseDTO;
+import com.gvendas.gestaovendas.dto.venda.VendaResponseDTO;
+import com.gvendas.gestaovendas.entidades.ItemVenda;
+import com.gvendas.gestaovendas.entidades.Venda;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AbstractVendaServico {
+    protected VendaResponseDTO criandoVendaResponseDTO(Venda venda, List<ItemVenda> itensVendaList) {
+        List<ItemVendaResponseDTO> itensVendaResponseDto = itensVendaList
+                .stream().map(this::criandoItensVendaResponseDto).collect(Collectors.toList());
+        return new VendaResponseDTO(venda.getCodigo(), venda.getData(), itensVendaResponseDto);
+    }
+
+    protected ItemVendaResponseDTO criandoItensVendaResponseDto(ItemVenda itemVenda) {
+        return new ItemVendaResponseDTO(itemVenda.getCodigo(), itemVenda.getQuantidade(), itemVenda.getPrecoVendido(),
+                itemVenda.getProduto().getCodigo(), itemVenda.getProduto().getDescricao());
+    }
+}
